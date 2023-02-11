179559f769902700eb5722c520e49860a2f65f22
Paweł Kmiecik <pkmiecik2105@gmail.com>
2023-02-11 21:15:02 +0100

    auth service unit tests with db repositories mocks

---
+++ flutter_project/test/services/auth_service_test.dart
@@ -0,0 +1,159 @@
+import 'package:flutter_project/apis/user_api.dart';
+import 'package:flutter_project/db/repositories/product_cache_repository.dart';
+import 'package:flutter_project/db/repositories/product_repository.dart';
+import 'package:flutter_project/db/repositories/user_repository.dart';
+import 'package:flutter_project/services/auth_service.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:flutter_project/models/user.dart';
+
+import '../../lib/db/collections/user.dart' as isar;
+import 'auth_service_test.mocks.dart';
+
+@GenerateMocks(
+    [UserApi, UserRepository, ProductRepository, ProductCacheRepository])
+void main() {
+  group('AuthService', () {
+    group('login', () {
+      test('throws exception if api throws exception', () {
+        final userApiMock = MockUserApi();
+        final userRepoMock = MockUserRepository();
+        final productRepoMock = MockProductRepository();
+        final productCacheMock = MockProductCacheRepository();
+        when(userApiMock.loginUser(any)).thenThrow(Exception());
+
+        final authService = AuthService(
+          userApiMock,
+          userRepoMock,
+          productRepoMock,
+          productCacheMock,
+        );
+
+        final response = authService.login('username', 'password');
+        expect(response, throwsException);
+      });
+      test('throws exception if repo throws exception', () {
+        final userApiMock = MockUserApi();
+        final userRepoMock = MockUserRepository();
+        final productRepoMock = MockProductRepository();
+        final productCacheMock = MockProductCacheRepository();
+        when(userRepoMock.addUser(any)).thenThrow(Exception());
+
+        final authService = AuthService(
+          userApiMock,
+          userRepoMock,
+          productRepoMock,
+          productCacheMock,
+        );
+
+        final response = authService.login('username', 'password');
+        expect(response, throwsException);
+      });
+      test('returns user with username, names and jwtToken', () async {
+        // setup mock users
+        const username = 'username1';
+        const password = 'password1';
+
+        final userApiResponse = User(
+          firstName: 'first name',
+          lastName: 'last name',
+          userName: username,
+          jwtToken: 'token',
+          apiId: '12',
+        );
+        //setup mock
+        final userApiMock = MockUserApi();
+        final userRepoMock = MockUserRepository();
+        final productRepoMock = MockProductRepository();
+        final productCacheMock = MockProductCacheRepository();
+        when(userApiMock.loginUser(any))
+            .thenAnswer((_) async => userApiResponse);
+        when(userRepoMock.addUser(any)).thenAnswer((_) async {});
+
+        final authService = AuthService(
+          userApiMock,
+          userRepoMock,
+          productRepoMock,
+          productCacheMock,
+        );
+
+        final response = await authService.login(username, password);
+        expect(response, userApiResponse);
+      });
+    });
+    group('logout', () {
+      test('throws exception if repository throws exception', () {
+        //setup mock
+        final userApiMock = MockUserApi();
+        final userRepoMock = MockUserRepository();
+        final productRepoMock = MockProductRepository();
+        final productCacheMock = MockProductCacheRepository();
+
+        // mock exception throwing
+        when(userRepoMock.deleteUser()).thenThrow(Exception());
+
+        final authService = AuthService(
+          userApiMock,
+          userRepoMock,
+          productRepoMock,
+          productCacheMock,
+        );
+
+        final response = authService.logout();
+        expect(response, throwsException);
+      });
+
+      test('returns false if any repo call is not successfull', () async {
+        //setup mock
+        final userApiMock = MockUserApi();
+        final userRepoMock = MockUserRepository();
+        final productRepoMock = MockProductRepository();
+        final productCacheMock = MockProductCacheRepository();
+
+        // mock exception throwing
+        when(userRepoMock.deleteUser()).thenAnswer((_) async => true);
+        when(productRepoMock.deleteAllProducts()).thenAnswer((_) async => true);
+        // one operation is not successfull
+        when(productCacheMock.deleteAllProductCache())
+            .thenAnswer((_) async => false);
+
+        final authService = AuthService(
+          userApiMock,
+          userRepoMock,
+          productRepoMock,
+          productCacheMock,
+        );
+
+        final response = await authService.logout();
+        expect(response, false);
+      });
+
+      test('returns true if all repo calls are successfull', () async {
+        //setup mock
+        final userApiMock = MockUserApi();
+        final userRepoMock = MockUserRepository();
+        final productRepoMock = MockProductRepository();
+        final productCacheMock = MockProductCacheRepository();
+
+        // mock exception throwing
+        when(userRepoMock.deleteUser()).thenAnswer((_) async => true);
+        when(productRepoMock.deleteAllProducts()).thenAnswer((_) async => true);
+        // one operation is not successfull
+        when(productCacheMock.deleteAllProductCache())
+            .thenAnswer((_) async => true);
+
+        final authService = AuthService(
+          userApiMock,
+          userRepoMock,
+          productRepoMock,
+          productCacheMock,
+        );
+
+        final response = await authService.logout();
+        expect(response, true);
+      });
+    });
+    group('register', () {});
+  });
+}
