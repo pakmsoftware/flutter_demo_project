1cc3fd5fb5fc6ccd1ab03795a5bc1cd08054aa02
Paweł Kmiecik <pkmiecik2105@gmail.com>
2023-02-11 22:25:14 +0100

    mapping from isar

--- flutter_project/test/models/product_test.dart
+++ flutter_project/test/models/product_test.dart
@@ -1,10 +1,48 @@
 import 'package:flutter_project/models/product.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+import 'package:flutter_project/db/collections/product.dart' as isar;
+
 void main() {
   group('Prouct', () {
     test(
       'correct mapping from Json',
+      () {
+        final isarProduct = isar.Product(
+          id: 12,
+          title: 'title',
+          description: 'description',
+          price: 27.60,
+          discountPercentage: 20.0,
+          rating: 4.5,
+          stock: 20,
+          brand: 'brand',
+          category: 'category',
+          thumbnail: 'thumbnail',
+          imageUrls: ['sada', 'adasd'],
+        );
+
+        final mappedProduct = Product.fromIsar(isarProduct);
+        expect(mappedProduct.id, isarProduct.id);
+        expect(mappedProduct.title, isarProduct.title);
+        expect(mappedProduct.description, isarProduct.description);
+        expect(mappedProduct.price, isarProduct.price);
+        expect(
+            mappedProduct.discountPercentage, isarProduct.discountPercentage);
+        expect(mappedProduct.rating, isarProduct.rating);
+        expect(mappedProduct.stock, isarProduct.stock);
+        expect(mappedProduct.brand, isarProduct.brand);
+        expect(mappedProduct.category, isarProduct.category);
+        for (var i = 0; i < mappedProduct.imageUrls.length; i++) {
+          final imageUrl = mappedProduct.imageUrls[i];
+          final jsonImageUrl = isarProduct.imageUrls[i];
+          expect(imageUrl, jsonImageUrl);
+        }
+      },
+    );
+
+    test(
+      'correct mapping from Isar',
       () {
         final Map<String, dynamic> json = {
           'id': 1,
